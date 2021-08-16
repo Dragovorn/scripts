8e39eb5397fbcca446280366c5033cecde9316af
Andrew Burr <me@dragovorn.com>
2021-08-15 23:48:43 -0400

    completely rewrite install.sh and create two new scripts, also, my main rig is archlinux now.

--- install.sh
+++ install.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-echo "Installing all files in cmd-line to /usr/lib/jvm..."
-cp -v -a cmd-line/. /usr/local/bin
-echo "Done! Scripts should be usable!"
+print() {
+  echo "install: $1"
+}
+
+die() {
+  print "$1"
+  exit
+}
+
+INSTALL_PATH="/usr/local/bin"
+SCRIPTS_DIR="./cmd-line"
+
+if [ ! -d "$SCRIPTS_DIR" ]; then
+  die "could not find installable scripts! is there a $SCRIPTS_DIR directory?"
+fi
+
+print "Found scripts directory: $SCRIPTS_DIR"
+
+for f in $SCRIPTS_DIR/*.sh
+do
+  print "running install script for: $f..."
+  $SCRIPTS_DIR/install-script.sh "$f" "$INSTALL_PATH"
+done
+
+print "done."
