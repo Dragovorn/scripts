6054158dfb708ea14e8632b3af1d0366ecd537c6
Andrew Burr <me@dragovorn.com>
2021-08-16 23:40:03 -0400

    fix issues with oh my zsh

--- cmd-line/aur-install.sh
+++ cmd-line/aur-install.sh
@@ -21,8 +21,8 @@ if [ -z "$AUR_INSTALL_DIR" ]; then
 
   AUR_INSTALL_DIR="${AUR_INSTALL_DIR/#~/$HOME}"
 
-  EXPORT="\"export AUR_INSTALL_DIR=\"$AUR_INSTALL_DIR\"  # Added by aur-install\" >> "
-  RC_FILE="$HOME/"
+  EXPORT="export AUR_INSTALL_DIR=\"$AUR_INSTALL_DIR\"  # Added by aur-install"
+  RC_FILE="$HOME"
 
 # If this ever needs to support more shells just add their respective detectors & rc file to this ladder.
 # (if this doesnt go python once i stop larping and writing this shit in shell and using nano as my editor)
@@ -34,25 +34,35 @@ if [ -z "$AUR_INSTALL_DIR" ]; then
     print "detected zsh, appending to $RC_FILE"
   fi
 
-  EXPORT="$EXPORT \"$RC_FILE\""
+  print "echo $EXPORT >> \"$RC_FILE\""
+  echo "$EXPORT" >> "$RC_FILE"
 
-  print "echo $EXPORT"
-  echo $EXPORT
-  source $RC_FILE
+# oh-my-zsh seems to have a weird bug that only presents itself if you source the .zshrc from a script
+# can't export from this script so ill just beg the user.
+  NEW_AUR_INSTALL=TRUE
 fi
 
 if [ ! -d "$AUR_INSTALL_DIR" ]; then
-  mkdir -p $AUR_INSTALL_DIR
+  mkdir -p "$AUR_INSTALL_DIR"
 fi
 
-PACKAGE=$1
-PACKAGE_DIR=$AUR_INSTALL_DIR/$PACKAGE
+PACKAGE="$1"
+PACKAGE_DIR="$AUR_INSTALL_DIR/$PACKAGE"
 AUR_URL="https://aur.archlinux.org/$PACKAGE.git"
 
-git clone $AUR_URL $PACKAGE_DIR
-
-cd $PACKAGE_DIR
+if [ -d "$PACKAGE_DIR" ]; then
+  print "$PACKAGE_DIR already exists! executing git pull..."
+  cd "$PACKAGE_DIR"
+  git pull
+else
+  git clone $AUR_URL "$PACKAGE_DIR"
+  cd "$PACKAGE_DIR"
+fi
 
 makepkg -sri
 
 print "done."
+
+if [ ! -z "$NEW_AUR_INSTALL" ]; then
+  print "because your installation is new please execute: source $RC_FILE"
+fi
